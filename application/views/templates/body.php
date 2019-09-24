228b0cc100256df83f66bfdd8c8bb3e912622bee
yuniarfabiputra <47998680+yuniarfabiputra@users.noreply.github.com>
2019-09-24 14:46:59 +0700

    h

--- application/views/templates/body.php
+++ application/views/templates/body.php
@@ -3,7 +3,7 @@
     <!-- END nav -->
 
     <section class="home-slider owl-carousel">
-        <div class="slider-item" style="background-image: url(images/kebun_kopi.jpg);">
+        <div class="slider-item" style="background-image: url(assets/images/kebun_kopi.jpg);" alt="">
             <div class="overlay"></div>
             <div class="container">
                 <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
@@ -35,7 +35,7 @@
             </div>
         </div>
 
-        <div class="slider-item" style="background-image: url(images/bg_3.jpg);">
+        <div class="slider-item" style="background-image: url(assets/images/bg_3.jpg);">
             <div class="overlay"></div>
             <div class="container">
                 <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
