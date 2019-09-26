16cb06adee44d8f65b4d84622b75927b6e6f99d2
yuniarfabiputra <47998680+yuniarfabiputra@users.noreply.github.com>
2019-09-26 22:24:15 +0700

    g

--- application/views/menu/berita.php
+++ application/views/menu/berita.php
@@ -20,7 +20,7 @@
         <div class="row d-flex">
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_1.jpg');">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/berita1.jpg');">
                     </a>
                     <div class="text py-4 d-block">
                         <div class="meta">
@@ -28,14 +28,15 @@
                             <div><a href="#">Admin</a></div>
                             <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
                         </div>
-                        <h3 class="heading mt-2"><a href="#">Coffee Testing Day</a></h3>
+                        <h3 class="heading mt-2"><a href="#">Wakil Ketua DPRD Kota Batu 'Kepincut' Kopi Bondowoso</a></h3>
                         <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                        <p><a href="#" class="btn btn-primary btn-outline-primary px-4 py-3">Baca Selengkapnya</a></p>
                     </div>
                 </div>
             </div>
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_2.jpg');">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/berita2.jpg');">
                     </a>
                     <div class="text py-4 d-block">
                         <div class="meta">
@@ -43,14 +44,15 @@
                             <div><a href="#">Admin</a></div>
                             <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
                         </div>
-                        <h3 class="heading mt-2"><a href="#">Coffee Testing Day</a></h3>
+                        <h3 class="heading mt-2"><a href="#">Nine Coffee, Kenalkan Kopi Bondowoso Melalui Franchise</a></h3>
                         <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                        <p><a href="#" class="btn btn-primary btn-outline-primary px-4 py-3">Baca Selengkapnya</a></p>
                     </div>
                 </div>
             </div>
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_3.jpg');">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/berita3.jpg');">
                     </a>
                     <div class="text py-4 d-block">
                         <div class="meta">
@@ -58,58 +60,14 @@
                             <div><a href="#">Admin</a></div>
                             <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
                         </div>
-                        <h3 class="heading mt-2"><a href="#">Coffee Testing Day</a></h3>
+                        <h3 class="heading mt-2"><a href="#">Kopi Terbaik Bondowoso Siap Semarakkan Festival Muharram 2016</a></h3>
                         <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                        <p><a href="#" class="btn btn-primary btn-outline-primary px-4 py-3">Baca Selengkapnya</a></p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_4.jpg');">
-                    </a>
-                    <div class="text py-4 d-block">
-                        <div class="meta">
-                            <div><a href="#">Sept 28, 2018</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading mt-2"><a href="#">Coffee Testing Day</a></h3>
-                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_5.jpg');">
-                    </a>
-                    <div class="text py-4 d-block">
-                        <div class="meta">
-                            <div><a href="#">Sept 28, 2018</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading mt-2"><a href="#">Coffee Testing Day</a></h3>
-                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_6.jpg');">
-                    </a>
-                    <div class="text py-4 d-block">
-                        <div class="meta">
-                            <div><a href="#">Sept 28, 2018</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading mt-2"><a href="#">Coffee Testing Day</a></h3>
-                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row mt-5">
+            
+        <div class="row mt-5 col text-center">
             <div class="col text-center">
                 <div class="block-27">
                     <ul>
--- application/views/menu/retail.php
+++ application/views/menu/retail.php
@@ -6,7 +6,7 @@
 			<div class="row slider-text justify-content-center align-items-center">
 
 				<div class="col-md-7 col-sm-12 text-center ftco-animate">
-					<h1 class="mb-3 mt-5 bread">Blog</h1>
+					<h1 class="mb-3 mt-5 bread">RETAIL</h1>
 					<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Blog</span></p>
 				</div>
 
