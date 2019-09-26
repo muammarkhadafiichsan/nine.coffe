4926a685d108dc57fc899f6025eb1f1b67a92429
muammarkhadafiichsan <47959678+muammarkhadafiichsan@users.noreply.github.com>
2019-09-26 20:25:05 +0700

    footer

--- application/views/menu/produk.php
+++ application/views/menu/produk.php
@@ -2,211 +2,134 @@
 
 
 
-<div class="slider-item" style="background-image: url(assets/images/bg_3.jpg);" data-stellar-background-ratio="0.5">
+  <div class="slider-item" style="background-image: url(assets/images/bg_3.jpg);" data-stellar-background-ratio="0.5">
 
     <div class="overlay"></div>
-  <div class="container">
-    <div class="row slider-text justify-content-center align-items-center">
+    <div class="container">
+      <div class="row slider-text justify-content-center align-items-center">
 
-      <div class="col-md-7 col-sm-12 text-center ftco-animate">
+        <div class="col-md-7 col-sm-12 text-center ftco-animate">
           <h1 class="mb-3 mt-5 bread">PRODUK</h1>
           <p class="breadcrumbs"><span class="mr-2"><a href="index.html">BERANDA</a></span> </p>
-      </div>
+        </div>
 
+      </div>
     </div>
   </div>
-</div>
 </section>
 
 
 <section class="ftco-menu mb-5 pb-5">
   <div class="container">
-      <div class="row d-md-flex">
-          <div class="col-lg-12 ftco-animate p-md-5">
-              <div class="row">
-            <div class="col-md-12 nav-link-wrap mb-5">
-              <div class="nav ftco-animate nav-pills justify-content-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                  <a class="nav-link active" id="v-pills-0-tab" data-toggle="pill" href="#v-pills-0" role="tab" aria-controls="v-pills-0" aria-selected="true">Robusta</a>
+    <div class="row d-md-flex">
+      <div class="col-lg-12 ftco-animate p-md-5">
+        <div class="row">
+          <div class="col-md-12 nav-link-wrap mb-5">
+            <div class="nav ftco-animate nav-pills justify-content-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+              <a class="nav-link active" id="v-pills-0-tab" data-toggle="pill" href="#v-pills-0" role="tab" aria-controls="v-pills-0" aria-selected="true">Robusta</a>
 
-                <a class="nav-link" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="false">Arabica</a>
+              <a class="nav-link" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="false">Arabica</a>
+            </div>
+          </div>
+          <div class="col-md-12 d-flex align-items-center">
+
+            <div class="tab-content ftco-animate" id="v-pills-tabContent">
+
+              <div class="tab-pane fade show active" id="v-pills-0" role="tabpanel" aria-labelledby="v-pills-0-tab">
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/kopi1.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/kopi2.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/kopi3.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/kopi4.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="tab-pane fade" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-1-tab">
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(images/menu-1.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(images/menu-2.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(images/menu-3.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(images/menu-4.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="col-md-12 d-flex align-items-center">
-              
-              <div class="tab-content ftco-animate" id="v-pills-tabContent">
-
-                <div class="tab-pane fade show active" id="v-pills-0" role="tabpanel" aria-labelledby="v-pills-0-tab">
-                    <div class="row">
-                        <div class="col-md-3">
-                                  <div class="menu-entry">
-                                          <a href="#" class="img" style="background-image: url(assets/images/kopi1.jpg);"></a>
-                                          <div class="text text-center pt-4">
-                                              <h3><a href="product-single.html">Coffee Capuccino</a></h3>
-                                              <p>A small river named Duden flows by their place and supplies</p>
-                                              
-                                          </div>
-                                      </div>
-                              </div>
-                              <div class="col-md-3">
-                                  <div class="menu-entry">
-                                          <a href="#" class="img" style="background-image: url(assets/images/kopi2.jpg);"></a>
-                                          <div class="text text-center pt-4">
-                                              <h3><a href="product-single.html">Coffee Capuccino</a></h3>
-                                              <p>A small river named Duden flows by their place and supplies</p>
-                                              
-                                          </div>
-                                      </div>
-                              </div>
-                              <div class="col-md-3">
-                                  <div class="menu-entry">
-                                          <a href="#" class="img" style="background-image: url(assets/images/kopi3.jpg);"></a>
-                                          <div class="text text-center pt-4">
-                                              <h3><a href="product-single.html">Coffee Capuccino</a></h3>
-                                              <p>A small river named Duden flows by their place and supplies</p>
-                                              
-                                          </div>
-                                      </div>
-                              </div>
-                              <div class="col-md-3">
-                                  <div class="menu-entry">
-                                          <a href="#" class="img" style="background-image: url(assets/images/kopi4.jpg);"></a>
-                                          <div class="text text-center pt-4">
-                                              <h3><a href="product-single.html">Coffee Capuccino</a></h3>
-                                              <p>A small river named Duden flows by their place and supplies</p>
-                                              
-                                          </div>
-                                      </div>
-                              </div>
-                    </div>
-                </div>
-
-                <div class="tab-pane fade" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-1-tab">
-                  <div class="row">
-                        <div class="col-md-3">
-                                  <div class="menu-entry">
-                                          <a href="#" class="img" style="background-image: url(images/menu-1.jpg);"></a>
-                                          <div class="text text-center pt-4">
-                                              <h3><a href="product-single.html">Coffee Capuccino</a></h3>
-                                              <p>A small river named Duden flows by their place and supplies</p>
-                                              
-                                          </div>
-                                      </div>
-                              </div>
-                              <div class="col-md-3">
-                                  <div class="menu-entry">
-                                          <a href="#" class="img" style="background-image: url(images/menu-2.jpg);"></a>
-                                          <div class="text text-center pt-4">
-                                              <h3><a href="product-single.html">Coffee Capuccino</a></h3>
-                                              <p>A small river named Duden flows by their place and supplies</p>
-                                              
-                                          </div>
-                                      </div>
-                              </div>
-                              <div class="col-md-3">
-                                  <div class="menu-entry">
-                                          <a href="#" class="img" style="background-image: url(images/menu-3.jpg);"></a>
-                                          <div class="text text-center pt-4">
-                                              <h3><a href="product-single.html">Coffee Capuccino</a></h3>
-                                              <p>A small river named Duden flows by their place and supplies</p>
-                                              
-                                          </div>
-                                      </div>
-                              </div>
-                              <div class="col-md-3">
-                                  <div class="menu-entry">
-                                          <a href="#" class="img" style="background-image: url(images/menu-4.jpg);"></a>
-                                          <div class="text text-center pt-4">
-                                              <h3><a href="product-single.html">Coffee Capuccino</a></h3>
-                                              <p>A small river named Duden flows by their place and supplies</p>
-                                              
-                                          </div>
-                                      </div>
-                              </div>
-                    </div>
-                </div>
-            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-</div>
+  </div>
+  </div>
 </section>
-
-<footer class="ftco-footer ftco-section img">
-  <div class="overlay"></div>
-<div class="container">
-  <div class="row mb-5">
-    <div class="col-lg-3 col-md-6 mb-5 mb-md-5">
-      <div class="ftco-footer-widget mb-4">
-        <h2 class="ftco-heading-2">About Us</h2>
-        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-        <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-          <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-          <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-          <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="col-lg-4 col-md-6 mb-5 mb-md-5">
-      <div class="ftco-footer-widget mb-4">
-        <h2 class="ftco-heading-2">Recent Blog</h2>
-        <div class="block-21 mb-4 d-flex">
-          <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
-          <div class="text">
-            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-            <div class="meta">
-              <div><a href="#"><span class="icon-calendar"></span> Sept 15, 2018</a></div>
-              <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-              <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-            </div>
-          </div>
-        </div>
-        <div class="block-21 mb-4 d-flex">
-          <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
-          <div class="text">
-            <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-            <div class="meta">
-              <div><a href="#"><span class="icon-calendar"></span> Sept 15, 2018</a></div>
-              <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-              <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-2 col-md-6 mb-5 mb-md-5">
-       <div class="ftco-footer-widget mb-4 ml-md-4">
-        <h2 class="ftco-heading-2">Services</h2>
-        <ul class="list-unstyled">
-          <li><a href="#" class="py-2 d-block">Cooked</a></li>
-          <li><a href="#" class="py-2 d-block">Deliver</a></li>
-          <li><a href="#" class="py-2 d-block">Quality Foods</a></li>
-          <li><a href="#" class="py-2 d-block">Mixed</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-6 mb-5 mb-md-5">
-      <div class="ftco-footer-widget mb-4">
-          <h2 class="ftco-heading-2">Have a Questions?</h2>
-          <div class="block-23 mb-3">
-            <ul>
-              <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-              <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-              <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
-            </ul>
-          </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12 text-center">
-
-      <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-    </div>
-  </div>
-</div>
-</footer>
