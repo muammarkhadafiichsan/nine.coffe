2c4cca0b2080538dae81fd0332666628a250d769
yuniarfabiputra <47998680+yuniarfabiputra@users.noreply.github.com>
2019-09-27 21:45:21 +0700

    h

--- application/views/tampilan/profil.php
+++ application/views/tampilan/profil.php
@@ -1,13 +1,13 @@
 <section class="home-slider owl-carousel">
 
-    <div class="slider-item" style="background-image: url(assets/images/kafe.jpeg);" data-stellar-background-ratio="0.5">
+    <div class="slider-item" style="background-image: url(assets/images/logo_nine.png);" alt="">
         <div class="overlay"></div>
         <div class="container">
             <div class="row slider-text justify-content-center align-items-center">
 
                 <div class="col-md-7 col-sm-12 text-center ftco-animate">
-                    <h1 class="mb-3 mt-5 bread">Cafe</h1>
-                    <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Beranda</a></span> </p>
+                    <h1 class="mb-3 mt-5 bread">NINE - COFFEE</h1>
+                    <p class="breadcrumbs"><span class="mr-2"><a href="index.html">COFFEE ROASTERY</a></span> </p>
                 </div>
 
             </div>
