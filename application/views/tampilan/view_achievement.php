80e90591eb6f0f7fd8aa09afea52118b5fcb557a
yuniarfabiputra <47998680+yuniarfabiputra@users.noreply.github.com>
2019-09-27 21:34:00 +0700

    k

--- application/views/tampilan/view_achievement.php
+++ application/views/tampilan/view_achievement.php
@@ -1,18 +1,11 @@
 <section class="home-slider owl-carousel">
-
-    <div class="slider-item" style="background-image: url(images/bg_3.jpg);" data-stellar-background-ratio="0.5">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row slider-text justify-content-center align-items-center">
-
-                <div class="col-md-7 col-sm-12 text-center ftco-animate">
-                    <h1 class="mb-3 mt-5 bread">Blog</h1>
-                    <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Blog</span></p>
+        <div class="slider-item" style="background-image: url(assets/images/logo_nine.png);" alt="">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
                 </div>
-
             </div>
         </div>
-    </div>
 </section>
 
 <section class="ftco-section">
@@ -20,33 +13,25 @@
         <div class="row d-flex">
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/berita1.jpg');">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/juara_satu.jpg');">
                     </a>
                     <div class="text py-4 d-block">
-                        <div class="meta">
-                            <div><a href="#">Sept 28, 2018</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading mt-2"><a href="#">Wakil Ketua DPRD Kota Batu 'Kepincut' Kopi Bondowoso</a></h3>
-                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                        <p><a href="#" class="btn btn-primary btn-outline-primary px-4 py-3">Baca Selengkapnya</a></p>
+                        
+                        <h3 class="heading mt-2"><a href="#">Juara 1 V60 BREWING COMPETITION Festival Kopi Nusantara 3 Bondowoso 2018</a></h3>
+                        <p>Dalam Festival Kopi Nusantara 3 Bondowoso Nine Coffee mendapatkan juara satu kategori V60 BREWING COMPETITION.</p>
+                        
                     </div>
                 </div>
             </div>
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/berita2.jpg');">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/juara_tiga.jpg');">
                     </a>
                     <div class="text py-4 d-block">
-                        <div class="meta">
-                            <div><a href="#">Sept 28, 2018</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading mt-2"><a href="#">Nine Coffee, Kenalkan Kopi Bondowoso Melalui Franchise</a></h3>
-                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                        <p><a href="#" class="btn btn-primary btn-outline-primary px-4 py-3">Baca Selengkapnya</a></p>
+                        
+                        <h3 class="heading mt-2"><a href="#">Juara 3 V60 BREWING COMPETITION Festival Kopi Nusantara 3 Bondowoso 2018</a></h3>
+                        <p>Dalam Festival Kopi Nusantara 3 Bondowoso Nine Coffee mendapatkan juara tiga kategori V60 BREWING COMPETITION.</p>
+                        
                     </div>
                 </div>
             </div>
@@ -55,14 +40,10 @@
                     <a href="blog-single.html" class="block-20" style="background-image: url('assets/images/berita3.jpg');">
                     </a>
                     <div class="text py-4 d-block">
-                        <div class="meta">
-                            <div><a href="#">Sept 28, 2018</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
-                        </div>
+                        
                         <h3 class="heading mt-2"><a href="#">Kopi Terbaik Bondowoso Siap Semarakkan Festival Muharram 2016</a></h3>
                         <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                        <p><a href="#" class="btn btn-primary btn-outline-primary px-4 py-3">Baca Selengkapnya</a></p>
+                        
                     </div>
                 </div>
             </div>
