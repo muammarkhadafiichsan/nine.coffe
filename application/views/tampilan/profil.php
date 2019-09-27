c582423e0581b1a4a78b6f53922e2dd73069beb1
yuniarfabiputra <47998680+yuniarfabiputra@users.noreply.github.com>
2019-09-27 23:36:36 +0700

    h

--- application/views/tampilan/profil.php
+++ application/views/tampilan/profil.php
@@ -1,16 +1,80 @@
 <section class="home-slider owl-carousel">
 
-    <div class="slider-item" style="background-image: url(assets/images/logo_nine.png);" alt="">
+      <div class="slider-item" style="background-image: url(images/bg_4.jpg);" data-stellar-background-ratio="0.5">
         <div class="overlay"></div>
         <div class="container">
-            <div class="row slider-text justify-content-center align-items-center">
+          <div class="row slider-text justify-content-center align-items-center">
 
-                <div class="col-md-7 col-sm-12 text-center ftco-animate">
-                    <h1 class="mb-3 mt-5 bread">NINE - COFFEE</h1>
-                    <p class="breadcrumbs"><span class="mr-2"><a href="index.html">COFFEE ROASTERY</a></span> </p>
+            <div class="col-md-7 col-sm-12 text-center ftco-animate">
+                <h1 class="mb-3 mt-5 bread">NINE COFFEE</h1>
+                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">COFFEE ROASTERY</a></span><p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-about d-md-flex">
+        <div class="one-half img mb-md-5 mt-5 ml-5 mr-5" style="background-image: url(assets/images/logo_nine.png);" width="1080px"></div>
+        <div class="one-half ftco-animate">
+            <div class="overlap">
+            <div class="heading-section ftco-animate ">
+              <h2 class="mb-4">Nine Coffee</h2>
+            </div>
+            <div>
+                    <p>Sejak dideklarasikan sebagai Republik Kopi pada Mei 2016 lalu, suasana baru terasa di Kabupaten Bondowoso, Jawa Timur. Kabupaten kecil berpenduduk 750 ribu jiwa lebih ini, punya hal baru yang menarik perhatian masyarakat. Ya, Kopi menjadi sesuatu yang sudah lama dikenal namun baru belakangan menjadi tren di Bondowoso.</p>
                 </div>
-
             </div>
         </div>
-    </div>
-</section>
+    </section>
+
+    <section class="ftco-section img" id="ftco-testimony" style="background-image: url(images/logo_nine.png);"  data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row justify-content-center mb-5">
+            <div class="col-md-7 heading-section text-center ftco-animate">
+              <h2 class="mb-4">Nine Coffee</h2>
+              <p>Sejak dideklarasikan sebagai Republik Kopi pada Mei 2016 lalu, suasana baru terasa di Kabupaten Bondowoso, Jawa Timur. Kabupaten kecil berpenduduk 750 ribu jiwa lebih ini, punya hal baru yang menarik perhatian masyarakat. Ya, Kopi menjadi sesuatu yang sudah lama dikenal namun baru belakangan menjadi tren di Bondowoso.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 pr-md-5">
+                    <div class="heading-section text-md-right ftco-animate">
+                <h2 class="mb-4">Nine Coffee</h2>
+                <p class="mb-4">Sejak dideklarasikan sebagai Republik Kopi pada Mei 2016 lalu, suasana baru terasa di Kabupaten Bondowoso, Jawa Timur. Kabupaten kecil berpenduduk 750 ribu jiwa lebih ini, punya hal baru yang menarik perhatian masyarakat. Ya, Kopi menjadi sesuatu yang sudah lama dikenal namun baru belakangan menjadi tren di Bondowoso.</p>
+                
+              </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="menu-entry">
+                               <a href="#" class="img" style="background-image: url(assets/images/bijikopi21.jpeg);"></a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="menu-entry mt-lg-4">
+                                <a href="#" class="img" style="background-image: url(assets/images/proses1.jpg);"></a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="menu-entry">
+                                <a href="#" class="img" style="background-image: url(assets/images/proses2.jpg);"></a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="menu-entry mt-lg-4">
+                                <a href="#" class="img" style="background-image: url(assets/images/proses3.jpg);"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
