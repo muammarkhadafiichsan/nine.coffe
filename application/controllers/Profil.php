e7ca25798937e1058402c7a1ab27169a4f4098be
yuniarfabiputra <47998680+yuniarfabiputra@users.noreply.github.com>
2019-09-28 22:06:40 +0700

    d

--- application/controllers/Profil.php
+++ application/controllers/Profil.php
@@ -8,10 +8,8 @@ class Profil extends CI_Controller
     public function index()
     {
         $this->load->view('templates/header');
-        $this->load->view('templates/header1');
         $this->load->view('templates/navbar');
         $this->load->view('tampilan/profil');
         $this->load->view('templates/footer');
-        $this->load->view('templates/footer1');
     }
 }
--- application/views/menu/produk.php
+++ application/views/menu/produk.php
@@ -18,137 +18,126 @@
   </div>
 </section>
 
-<body>
-<div class="wrapper">
-    <form action="" id="wizard">
-      <!-- SECTION 1 -->
-      <h4></h4>
-      <section>
-        <div class="inner">
-          <div class="image-holder">
-            <img src="assets/images/produk2.jpg" alt="">
+
+<section class="ftco-menu mb-5 pb-5">
+  <div class="container">
+    <div class="row d-md-flex">
+      <div class="col-lg-12 ftco-animate p-md-5">
+        <div class="row">
+          <div class="col-md-12 nav-link-wrap mb-5">
+            <div class="nav ftco-animate nav-pills justify-content-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+              <a class="nav-link active" id="v-pills-0-tab" data-toggle="pill" role="tab" aria-controls="v-pills-0" aria-selected="true">Signature NineCoffee</a>
+            </div>
           </div>
-          <div class="form-content">
-            <div class="form-inner">
-              <div class="form-header">
-                <h3>Book a table</h3>
-                <p>~ Check out our place ~</p>
-              </div>
-              <div class="form-row" style="margin-bottom: 31px;">
-                <label for="">
-                  <i class="zmdi zmdi-account-o"></i>
-                  People
-                </label>
-                <div class="select">
-                  <div class="form-holder">
-                    <div class="select-control"></div>
-                    <i class="zmdi zmdi-chevron-down"></i>
+          <div class="col-md-12 d-flex align-items-center">
+
+            <div class="tab-content ftco-animate" id="v-pills-tabContent">
+
+              <div class="tab-pane fade show active" id="v-pills-0" role="tabpanel" aria-labelledby="v-pills-0-tab">
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/produk2.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">ARABIKA IJEN SPECIALTY 'BEANS'</a></h3>
+                        <p></p>
+
+                      </div>
+                    </div>
                   </div>
-                  <ul class="dropdown">
-                    <li rel="1">1</li>
-                    <li rel="2">2</li>
-                    <li rel="3">3</li>
-                    <li rel="4">4</li>
-                    <li rel="5">5</li>
-                  </ul>
-                </div>
-              </div>
-              <div class="form-row" style="margin-bottom: 31px;">
-                <label for="">
-                  <i class="zmdi zmdi-calendar-check"></i>
-                  Date
-                </label>
-                <input type='text' class='datepicker-here form-control' data-language='en' id="dp"
-                  data-date-format="dd M yyyy" />
-              </div>
-              <div class="form-row">
-                <label for="" style="margin-bottom: 9px;">
-                  <i class="zmdi zmdi-alarm"></i>
-                  Time
-                </label>
-                <div class="form-holder select-holder">
-                  <select name="" id="" class="form-control">
-                    <option value="9am"><span>9</span> am</option>
-                    <option value="10am"><span>10</span> am</option>
-                    <option value="11am"><span>11</span>am</option>
-                    <option value="12am"><span>12</span>am</option>
-                  </select>
-                  <i class="zmdi zmdi-caret-down"></i>
-                </div>
-                <div class="form-holder select-holder">
-                  <select name="" id="" class="form-control">
-                    <option value="7pm"><span>7</span> pm</option>
-                    <option value="8pm"><span>8</span> pm</option>
-                    <option value="9pm"><span>9</span> pm</option>
-                    <option value="10pm"><span>10</span> pm</option>
-                  </select>
-                  <i class="zmdi zmdi-caret-down"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/produk3.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">ARABIKA IJEN BONDOWOSO</a></h3>
+                        <p></p>
 
-      <!-- SECTION 2 -->
-      <h4></h4>
-      <section>
-        <div class="inner">
-          <div class="image-holder">
-            <img src="assets/images/produk3.jpg" alt="">
-          </div>
-          <div class="form-content">
-            <div class="form-inner">
-              <div class="form-header">
-                <h3>Contact details</h3>
-                <p>~ Check out our place ~</p>
-              </div>
-              <div class="form-row">
-                <label for="">
-                  <i class="zmdi zmdi-account-o"></i>
-                  Name
-                </label>
-                <input type="text" class="form-control" placeholder="Name">
-              </div>
-              <div class="form-row">
-                <label for="">
-                  <i class="zmdi zmdi-phone"></i>
-                  Phone
-                </label>
-                <input type="text" class="form-control" placeholder="My phone">
-              </div>
-              <div class="form-row">
-                <label for="">
-                  <i class="zmdi zmdi-email"></i>
-                  Email
-                </label>
-                <input type="text" class="form-control" placeholder="Your Email">
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/produk4.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">ARABIKA IJEN SPECIALTY</a></h3>
+                        <p></p>
 
-      <!-- SECTION 3 -->
-      <h4></h4>
-      <section>
-        <div class="inner">
-          <div class="image-holder">
-            <img src="assets/images/produk4.jpg" alt="">
-          </div>
-          <div class="form-content">
-            <div class="form-inner form-inner-last">
-              <div class="ready">
-                <span>
-                  <i class="zmdi zmdi-check"></i>
-                </span>
-                <p class="text-1">Your table is ready</p>
-                <p class="text-2">~ Happy friends with a say ~</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/produk5.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">ROBUSTA IJEN BONDOWOSO</a></h3>
+                        <p></p>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="tab-pane fade" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-1-tab">
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/produk1.jpeg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Specialty Arabica</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/produk2.jpeg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Arabica Peaberry Ijen Bondowoso</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/produk3.jpeg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Arabica Civet Coffee</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(images/menu-4.jpg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino1</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="menu-entry">
+                      <a href="#" class="img" style="background-image: url(assets/images/produk3.jpeg);"></a>
+                      <div class="text text-center pt-4">
+                        <h3><a href="product-single.html">Coffee Capuccino</a></h3>
+                        <p>A small river named Duden flows by their place and supplies</p>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </form>
+      </div>
+    </div>
   </div>
-  </body>
+  </div>
+  </div>
+</section>
