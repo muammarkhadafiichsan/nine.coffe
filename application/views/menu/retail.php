f2794543b719b0bdc2bda44a089cbfa80f487619
muammarkhadafiichsan <47959678+muammarkhadafiichsan@users.noreply.github.com>
2019-09-28 20:24:52 +0700

    bootstrap

--- application/views/menu/retail.php
+++ application/views/menu/retail.php
@@ -5,28 +5,46 @@
 			<div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
 
 				<div class="col-md-8 col-sm-12 text-center ">
-					<span class="subheading"></span>
+
 					<h1 class="mb-4">SEKECA KOFFIE</h1>
-					<p class="mb-4 mb-md-5">SEKECA KOFFIE adalah cafe yang terletak di kota jember. Kedai ini juga menggunakan biji kopi dari NINE C0FFEE ROASTERY. </p>
-					<p> <a href="https://www.instagram.com/sekeca.koffie/" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">INSTAGRAM</a></p>
+					<p class="mb-4 mb-md-5">Salah satu kedai terkenal di Jember ini disuplay langsung oleh NINE COFFEE dari biji kopi ijen. Sehingga terjamin cita rasanya.</p>
+					<p> <a href="https://www.instagram.com/sekeca.koffie/" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">Official Instagram</a></p>
+
 				</div>
 
 			</div>
 		</div>
 	</div>
 
-	<div class="slider-item" style="background-image: url(assets/images/colors_pub.jpeg);">
+	<div class="slider-item" style="background-image: url(assets/images/kafe_edit.jpeg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
+
+				<div class="col-md-8 col-sm-12 text-center ftco-animate">
+
+					<h1 class="mb-4">NINE COFFEE</h1>
+					<p class="mb-4 mb-md-5">NINE COFFEE terletak di jantung kota bondowoso ini menyajikan kopi terbaik di bondowoso. Tempat yang enak dan nyaman dan murah ini membuat cafe ini tidak pernah sepi.</p>
+					<p> <a href="https://www.instagram.com/sekeca.koffie/" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">Official Instagram</a></p>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="slider-item" style="background-image: url(assets/images/proses2.jpg);">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
 
 				<div class="col-md-8 col-sm-12 text-center ftco-animate">
 					<span class="subheading">Selamat Datang</span>
-					<h1 class="mb-4">Proses Pengolahan Kopi Pasca Panen</h1>
-					<p class="mb-4 mb-md-5">Setelah biji kopi dipanen, kemudian diproses di pabrik untuk disortir kembali untuk dipilih biji kopi terbaik.</p>
-					<p><a href="#" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">Baca Selengkapnya</a></p>
+					<h1 class="mb-4">Pabrik Bersih</h1>
+					<p class="mb-4 mb-md-5">. </p>
+
 				</div>
 
 			</div>
 		</div>
 	</div>
+</section>
