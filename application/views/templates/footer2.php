bc622c753839543fb11a91e196f1a49a5da9175b
yuniarfabiputra <47998680+yuniarfabiputra@users.noreply.github.com>
2019-09-29 14:33:21 +0700

    g

---
+++ application/views/templates/footer2.php
@@ -0,0 +1,17 @@
+<script src="<?= base_url('assets2/'); ?>js/jquery.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/popper.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/bootstrap.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/jquery.easing.1.3.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/jquery.waypoints.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/jquery.stellar.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/owl.carousel.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/jquery.magnific-popup.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/aos.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/jquery.animateNumber.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/bootstrap-datepicker.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/jquery.timepicker.min.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/scrollax.min.js"></script>
+ 
+  <script src="<?= base_url('assets2/'); ?>js/google-map.js"></script>
+  <script src="<?= base_url('assets2/'); ?>js/main.js"></script>
---
+++ application/views/templates/header2.php
@@ -0,0 +1,24 @@
+
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/animate.css">
+    
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/magnific-popup.css">
+
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/aos.css">
+
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/ionicons.min.css">
+
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/flaticon.css">
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/icomoon.css">
+    <link rel="stylesheet" href="<?= base_url('assets2/'); ?>css/style.css">
