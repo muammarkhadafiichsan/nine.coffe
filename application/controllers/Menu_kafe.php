74b208b4af37db5deb1465cce24e561474ba9d2f
yuniarfabiputra <47998680+yuniarfabiputra@users.noreply.github.com>
2019-09-29 15:23:41 +0700

    Update Menu_kafe.php

--- application/controllers/Menu_kafe.php
+++ application/controllers/Menu_kafe.php
@@ -6,11 +6,11 @@ class Menu_Kafe extends CI_Controller
 
     public function index()
     {
-    	$this->load->view('templates/header2');
+
         $this->load->view('templates/header');
         $this->load->view('templates/navbar');
         $this->load->view('menu/kafe');
         $this->load->view('templates/footer');
-        $this->load->view('templates/footer2');
+        
     }
 }
